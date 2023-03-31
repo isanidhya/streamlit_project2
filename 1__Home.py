f6fe964b65f6d5527131e4282207a79a11f36e92
Sanidhya Sahu <115490812+isanidhya@users.noreply.github.com>
2023-03-31 20:05:19 +0530

    Add files via upload

---
+++ 1__Home.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.markdown("<h2 style='background-color:red;color:white;padding:10px;border-radius:12px;'>SAMRAT ASHOK TECHNOLOGICAL INSTITUTE</h2>",unsafe_allow_html= True)
+
+"""
+### Director Message
+
+"""
+cols = st.columns([1,2,1])
+cols[1].image(r"http://satiengg.in/images/BOG_Director.jpg")
+"""
+###### Dr R K Pandit, Director MITS, Gwalior & Officiating Director SATI , Vidisha. Welcome all to Socially Sustainable Premier Technical Institute of Madhya Pradesh.
+"""
+"""
+### Vision
+To contribute towards service and development of the mankind, through quality education and research in the area of Science, Technology and Management.
+
+### Mission
+To create quality manpower equipped with technical skills, social values, leadership, creativity, and innovation for the benefits, betterment of mankind and sustainable development of the nation.
+
+"""
